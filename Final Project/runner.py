61bd5b43c0fc8441f03a10bf176975a1929a27bd
Jdemore <Jdemore4@kent.edu>
2025-05-07 15:04:07 -0400

    python Black formatting

--- Final Project/runner.py
+++ Final Project/runner.py
@@ -8,13 +8,14 @@ from parser import parse
 
 from evaluator import evaluate
 
+
 def main():
     environment = {}
-    
+
     # Check for command line arguments
     if len(sys.argv) > 1:
         # Filename provided, read and execute it
-        with open(sys.argv[1], 'r') as f:
+        with open(sys.argv[1], "r") as f:
             source_code = f.read()
         try:
             tokens = tokenize(source_code)
@@ -23,16 +24,15 @@ def main():
         except Exception as e:
             print(f"Error: {e}")
 
-
     else:
         # REPL loop
         while True:
             try:
                 # Read input
-                source_code = input('>> ')
+                source_code = input(">> ")
 
                 # Exit condition for the REPL loop
-                if source_code.strip() in ['exit', 'quit']:
+                if source_code.strip() in ["exit", "quit"]:
                     break
 
                 # Tokenize, parse, and execute the code
@@ -42,6 +42,6 @@ def main():
             except Exception as e:
                 print(f"Error: {e}")
 
+
 if __name__ == "__main__":
     main()
-
